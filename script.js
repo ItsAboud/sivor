acd0c785009f20f14d8130b5daf85b1f1fc143fc
ItsAboud <147438600+ItsAboud@users.noreply.github.com>
2024-07-18 11:59:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let menuIcon = document.querySelector("#menu-icon")
-let navbar = document.querySelector(".navbar")
+document.addEventListener('DOMContentLoaded', function() {
+    let menuIcon = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
-}
+    menuIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle("bx-x");
+        navbar.classList.toggle("active");
+    });
+});
